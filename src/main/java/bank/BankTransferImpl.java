27eb9b22597531364ddf2695b1c36bec7639ec94
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2024-11-14 12:26:04 +0100

    Update BankTransferImpl.java

--- src/main/java/bank/BankTransferImpl.java
+++ src/main/java/bank/BankTransferImpl.java
@@ -4,18 +4,19 @@ public class BankTransferImpl implements BankTransfer{
 
 	private final int fee = 1;
 	
-	@Override
-	public void transfer(BankAccount from, BankAccount to, int amount) {
-		
-		if(from.getBalance() < amount + fee)
-			throw new IllegalArgumentException("You have no enought money!!!");
-		
-		from.withdraw(amount + fee);
-		
-		to.deposit(amount);
-	}
-	
-	public BankAccount openAccount(int balance) {
-		return new BankAccount(balance);
-	}
+    @Override
+    public void transfer(BankAccount from, BankAccount to, int amount) {
+        if (from.getBalance() < amount) {
+            throw new IllegalArgumentException("You have no enough money!!!");
+        }
+
+        from.withdraw(amount);
+        to.deposit(amount);
+    }
+
+    public BankAccount openAccount(int balance) {
+        return new BankAccount(balance);
+    }
 }
+
+
